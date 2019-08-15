e5eafd80524053f563246edb6867dec22455ce8d
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-08-16 00:04:10 +0300

    implemented demo project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+
 import React from 'react';
-import logo from './logo.svg';
+
+import ErrorBoundary from './components/ErrorBoundary';
+import BuggyCounter from './components/BuggyCounter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <b>
+          This is an example of error boundaries in React 16.
+          <br /><br />
+          Click on the numbers to increase the counters.
+          <br />
+          The counter is programmed to throw when it reaches 5. This simulates a JavaScript error in a component.
+        </b>
+      </p>
+      <hr />
+      <ErrorBoundary>
+        <p>These two counters are inside the same error boundary. If one crashes, the error boundary will replace both of them.</p>
+        <BuggyCounter />
+        <BuggyCounter />
+      </ErrorBoundary>
+      <hr />
+      <p>These two counters are each inside of their own error boundary. So if one crashes, the other is not affected.</p>
+      <ErrorBoundary><BuggyCounter /></ErrorBoundary>
+      <ErrorBoundary><BuggyCounter /></ErrorBoundary>
     </div>
   );
 }
